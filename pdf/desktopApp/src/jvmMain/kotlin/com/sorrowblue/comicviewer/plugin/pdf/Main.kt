514be1efc98545bcb6b9acc742f2ea90b64bcacc
SorrowBlue <sorrowblue.dev@gmail.com>
2026-05-13 12:02:22 +0900

    Remove unused coroutine delay from main application function

--- pdf/desktopApp/src/jvmMain/kotlin/com/sorrowblue/comicviewer/plugin/pdf/Main.kt
+++ pdf/desktopApp/src/jvmMain/kotlin/com/sorrowblue/comicviewer/plugin/pdf/Main.kt
@@ -5,10 +5,6 @@ import androidx.compose.ui.window.application
 import comicviewerplugin.pdf.generated.resources.Res as PdfRes
 import comicviewerplugin.pdf.generated.resources.ic_product
 import dev.zacsweers.metro.createGraphFactory
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
 
 fun main() = application {
@@ -21,9 +17,5 @@ fun main() = application {
         icon = painterResource(PdfRes.drawable.ic_product),
     ) {
         graph.pdfPluginAppClass()
-    }
-    CoroutineScope(Dispatchers.Main).launch {
-        delay(1000)
-        println("delay(1000)")
     }
 }
