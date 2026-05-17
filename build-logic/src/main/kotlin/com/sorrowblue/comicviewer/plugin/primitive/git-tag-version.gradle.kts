573dfea4fe82e0f671a753e50b3b7c2dd6b3913e
SorrowBlue <sorrowblue.dev@gmail.com>
2026-05-18 01:44:28 +0900

    Update build configuration to include conveyor-build2 and set version to 1.0.0

--- build-logic/src/main/kotlin/com/sorrowblue/comicviewer/plugin/primitive/git-tag-version.gradle.kts
+++ build-logic/src/main/kotlin/com/sorrowblue/comicviewer/plugin/primitive/git-tag-version.gradle.kts
@@ -3,7 +3,8 @@ package com.sorrowblue.comicviewer.plugin.primitive
 import java.io.ByteArrayOutputStream
 
 private val gitTagProvider = providers.of(GitTagValueSource::class) {}
-version = formatVersion(gitTagProvider.get())
+//version = formatVersion(gitTagProvider.get())
+version = "1.0.0"
 
 interface GitTagParameters : ValueSourceParameters
 
